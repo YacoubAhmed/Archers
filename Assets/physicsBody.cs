d83378240aed8c1a773e909e37378c27f31c8ac8
Yacoub Ahmed <yacoub.ahmedy@gmail.com>
2016-11-23 12:13:07 +0000

    Update physicsBody.cs

--- Assets/physicsBody.cs
+++ Assets/physicsBody.cs
@@ -21,9 +21,24 @@ public class physicsBody : MonoBehaviour {
 	void Update () {
 		if (!sleeping) {
 			foreach (attractor at in attractors) {
+				//using newtons law of gravity,
+				//Fgrav = G * m1 * m2 / r^2
+				//The force due to gravity is equal to
+				//the gravitational constant (normally 6.674x10^-11 but i have just increased it to 6.674
+				//to reduce floating point rounding errors, while also making the need for large masses on the
+				//planets much less (baisically removing all of the powers of 10 to make maths easier))
+				//multiplied by the mass of the first object (the arrow)
+				//multiplied by the mass of the second object (the attractors mass)
+				//and divided by the distance between these two objects (calculated by math.squareDist)
 				Vector2 dir = at.transform.position - transform.position;
+				//getting the direction in which we should apply the force by subtracting the current position vector
+				//of the arrow from the current position vector of the planet
 				dir = dir.normalized;
+				//normalising the direction vector so that its magnitude is only 1. This will make the direciton
+				//vector only be a direction and have no effect on the magnitude of the force that acts, since its
+				//magnitude is only 1.
 				float magnitude = 6.674f;
+				//define a float magnitude to have the previously mentioned universal gravitational field constant
 				float dist = math.squareDist (transform.position, at.transform.position);
 				magnitude *= rb.mass;
 				magnitude *= at.mass;
