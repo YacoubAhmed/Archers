e8e66c4eec3a9f0135c1cc0d2a1641ad877a8488
Yacoub Ahmed <yacoub.ahmedy@gmail.com>
2016-12-14 09:13:50 +0000

    Update attractor.cs

--- Assets/attractor.cs
+++ Assets/attractor.cs
@@ -1,20 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+//the attractor class is used to find each different celestial body so that the physicsbody scripts know in which direction to add
+//forces to simulate gravity
 public class attractor : MonoBehaviour {
-
+	//two floats, mass and radius, used to determine both the edge of the planet and the mass of planet, so we can calculate the
+	//magnitude of the force due to gravity we need to add to each physicsbody
 	public float mass;
 	public float radius;
 
 	// Use this for initialization
 	void Start () {
+		//setting the variable radius to the scale of this planet
 		radius = transform.localScale.x;
-		GetComponent<Rigidbody2D> ().mass = 25 * radius;
+		//using the equation for volume of a sphere:
+		//V = 4/3 * Pi * r^3
+		//we can determine the mass of this object like so:
+		GetComponent<Rigidbody2D> ().mass = 4/3 * Mathf.PI * radius * radius * radius;
+		//setting this script's mass variable to the calculated one
 		mass = GetComponent<Rigidbody2D>().mass;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
